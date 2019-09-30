1d608a021bf32fca42e9c4ccda5b4825b539bcbc
cstew2006 <53883455+cstew2006@users.noreply.github.com>
2019-09-30 06:28:04 -0400

    Add files via upload

---
+++ inc/generate_questions.php
@@ -0,0 +1,53 @@
+<?php
+// Generate random questions
+$questions = [];
+$operators = [" * ", " + ", " - "];
+$pick = rand(0, 2);
+// Loop for required number of questions
+
+for ($i = 0; $i <= 9; $i++) {
+
+// Get random numbers to add
+    $a = rand(1, 100);
+    $b = rand(1, 100);
+
+// Calculate correct answer //Used Jospehs code for this part to asssit with writing my own
+switch($operators[$pick]) {
+        case " * ":
+          $answer = $a + $b;
+          break;
+        case " + ":
+          $answer = $a - $b;
+          break;
+        case " - ":
+          $answer = $a * $b;
+          break;
+       }
+        if (abs($answer) > 10 || $answer == 0) {
+        $wrongAnswer1 = $answer + rand(-10, 10);
+        $wrongAnswer2 = $answer + rand(-10, 10);
+        while ($wrongAnswer1 == $answer || $wrongAnswer1 == $wrongAnswer2) {
+        $wrongAnswer1 = $answer + rand(-10, 10);
+  }
+        while ($wrongAnswer2 == $answer) {
+        $wrongAnswer2 = $answer + rand(-10, 10);
+  }
+    } elseif ($answer >= -10 && $answer < 0) {
+        $wrongAnswer1 = $answer + rand(-10, -1);
+        $wrongAnswer2 = $answer + rand(-10, -1);
+        while ($wrongAnswer1 == $wrongAnswer2) {
+       $wrongAnswer = $answer + rand(-10, -1);
+  }
+    } elseif ($answer > 0 && $answer <= 10) {
+        $wrongAnswer1 = $answer + rand(1, 10);
+        $wrongAnswer2 = $answer + rand(1, 10);
+        while ($wrongAnswer1 == $wrongAnswer2) {
+        $wrongAnswer1 = $answer + rand(1, 10);
+  }
+    }
+        $questions[$i]["leftOperand"] = $a;
+        $questions[$i]["rightOperand"] = $b;
+        $questions[$i]["correctAnswer"] = $answer;
+        $questions[$i]["firstIncorrectAnswer"] = $wrongAnswer1;
+        $questions[$i]["secondIncorrectAnswer"] = $wrongAnswer2;
+}
---
+++ inc/quiz.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * PHP Techdegree Project 2: Build a Quiz App in PHP
+ *
+ * These comments are to help you get started.
+ * You may split the file and move the comments around as needed.
+ *
+ * You will find examples of formating in the index.php script.
+ * Make sure you update the index file to use this PHP script, and persist the users answers.
+ *
+ * For the questions, you may use:
+ *  1. PHP array of questions
+ *  2. json formated questions
+ *  3. auto generate questions
+ *
+ */
+ session_start();
+
+// Include questions
+include 'generate_questions.php';
+
+// Keep track of which questions have been asked
+$page = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_NUMBER_INT);
+if (empty($page)) {
+    session_destroy();
+    $page = 1;
+}
+if (!isset($_SESSION['score'])) {
+    $_SESSION['score'] = 0;
+}
+// Show which question they are on
+echo '<p class="breadcrumbs">Question ' . $page . ' of ' . $total . ' </p>';
+   echo '<p class="quiz">What is ' . $questions[$page - 1]['leftAdder'] . ' + ' . $questions[$page - 1]['rightAdder'] . '?</p>';
+   echo '<form action="index.php?p=' . ($page + 1) . '" method="post">';
+   echo '<input type="hidden" name="correctAnswer" value="' . $questions[$page - 1]['correctAnswer'] . '" />';
+
+// Show random question
+  echo '<div id="quiz-box">';
+
+// Shuffle answer buttons
+function checkAnswer() {
+    $answers = [];
+    if (isset($_POST['answer1'])) {
+        $_SESSION['answer1'] = filter_input(INPUT_POST, 'answer1', FILTER_SANITIZE_NUMBER_INT);
+        $answers = [$_SESSION['answer1']];
+    } elseif (isset($_POST['answer2'])) {
+        $_SESSION['answer2'] = filter_input(INPUT_POST, 'answer2', FILTER_SANITIZE_NUMBER_INT);
+        $answers = [$_SESSION['answer2']];
+    } elseif (isset($_POST['answer3'])) {
+        $_SESSION['answer3'] = filter_input(INPUT_POST, 'answer3', FILTER_SANITIZE_NUMBER_INT);
+        $answers = [$_SESSION['answer3']];
+    }
+    if (isset($_POST['correct'])) {
+        $_SESSION['correct'] = filter_input(INPUT_POST, 'correct', FILTER_SANITIZE_NUMBER_INT);
+    }
+    foreach ($answers as $answer) {
+        if ($answer == $_SESSION['correct']) {
+            echo "<p class='breadcrumbs'>Correct!</p>";
+            ++$_SESSION['score'];
+        } else {
+            echo "<p class='breadcrumbs'>That is incorrect! The right answer was " . $_SESSION['correct'] . ".</p>";
+        }
+    }
+    return $_SESSION['score'];
+}
+function restart() {
+    echo "<form action='index.php' method='post'>";
+    echo "<input type='submit' class='btn' name='restart' value='Take the quiz again'>";
+    echo "</form>";
+}
+
+// Shows the score
+function score() {
+    $score = checkAnswer();
+    echo "<p class='quiz'>Your final score is " . $score . " out of 10.</p>";
