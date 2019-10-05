85ebb875776f054c441bb5404e7fb307de104661
cstew2006 <53883455+cstew2006@users.noreply.github.com>
2019-10-04 23:34:13 -0400

    Update generate_questions.php

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -1,53 +1,35 @@
 <?php
 // Generate random questions
 $questions = [];
-$operators = [" * ", " + ", " - "];
-$pick = rand(0, 2);
 // Loop for required number of questions
-
 for ($i = 0; $i <= 9; $i++) {
-
-// Get random numbers to add
-    $a = rand(1, 100);
-    $b = rand(1, 100);
-
-// Calculate correct answer //Used Jospehs code for this part to asssit with writing my own
-switch($operators[$pick]) {
-        case " * ":
-          $answer = $a + $b;
-          break;
-        case " + ":
-          $answer = $a - $b;
-          break;
-        case " - ":
-          $answer = $a * $b;
-          break;
-       }
-        if (abs($answer) > 10 || $answer == 0) {
-        $wrongAnswer1 = $answer + rand(-10, 10);
-        $wrongAnswer2 = $answer + rand(-10, 10);
-        while ($wrongAnswer1 == $answer || $wrongAnswer1 == $wrongAnswer2) {
-        $wrongAnswer1 = $answer + rand(-10, 10);
-  }
-        while ($wrongAnswer2 == $answer) {
-        $wrongAnswer2 = $answer + rand(-10, 10);
-  }
-    } elseif ($answer >= -10 && $answer < 0) {
-        $wrongAnswer1 = $answer + rand(-10, -1);
-        $wrongAnswer2 = $answer + rand(-10, -1);
-        while ($wrongAnswer1 == $wrongAnswer2) {
-       $wrongAnswer = $answer + rand(-10, -1);
-  }
-    } elseif ($answer > 0 && $answer <= 10) {
-        $wrongAnswer1 = $answer + rand(1, 10);
-        $wrongAnswer2 = $answer + rand(1, 10);
-        while ($wrongAnswer1 == $wrongAnswer2) {
-        $wrongAnswer1 = $answer + rand(1, 10);
-  }
+    // Get random numbers to add
+    $leftAdder = rand(1, 100);
+    $rightAdder = rand(1, 100);
+    // Calculate correct answer
+    $correctAnswer = $leftAdder + $rightAdder;
+  
+  
+    // Get incorrect answers within 10 numbers either way of correct answer
+    // Make sure it is a unique answer
+    $firstIncorrectAnswer = abs($correctAnswer + rand(-10, 10));
+    while ($correctAnswer == $firstIncorrectAnswer) {
+        $firstIncorrectAnswer = abs($correctAnswer + rand(-10, 10));
     }
-        $questions[$i]["leftOperand"] = $a;
-        $questions[$i]["rightOperand"] = $b;
-        $questions[$i]["correctAnswer"] = $answer;
-        $questions[$i]["firstIncorrectAnswer"] = $wrongAnswer1;
-        $questions[$i]["secondIncorrectAnswer"] = $wrongAnswer2;
+    $secondIncorrectAnswer = abs($correctAnswer + rand(-10, 10));
+    while ($firstIncorrectAnswer == $secondIncorrectAnswer || $correctAnswer == $secondIncorrectAnswer) {
+        $secondIncorrectAnswer = abs($correctAnswer + rand(-10, 10));
+    }
+  
+  
+  
+    // Add question and answer to questions array
+    $questions[] = [
+        'leftAdder' => $leftAdder,
+        'rightAdder' => $rightAdder,
+        'correctAnswer' => $correctAnswer,
+        'firstIncorrectAnswer' => $firstIncorrectAnswer,
+        'secondIncorrectAnswer' => $secondIncorrectAnswer
+    ];
 }
+?>
