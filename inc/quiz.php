9b22e9ff7164f52220d5bd9a8341277b9cc337ea
cstew2006 <53883455+cstew2006@users.noreply.github.com>
2019-10-04 23:29:59 -0400

    Update quiz.php

--- inc/quiz.php
+++ inc/quiz.php
@@ -1,78 +1,71 @@
 <?php
-/*
- * PHP Techdegree Project 2: Build a Quiz App in PHP
- *
- * These comments are to help you get started.
- * You may split the file and move the comments around as needed.
- *
- * You will find examples of formating in the index.php script.
- * Make sure you update the index file to use this PHP script, and persist the users answers.
- *
- * For the questions, you may use:
- *  1. PHP array of questions
- *  2. json formated questions
- *  3. auto generate questions
- *
- */
- session_start();
-
+session_start();
+if (!isset($_SESSION['amountCorrect'])) {
+    $_SESSION['amountCorrect'] = 0;
+} 
 // Include questions
 include 'generate_questions.php';
-
-// Keep track of which questions have been asked
+// var_dump($questions);
 $page = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_NUMBER_INT);
 if (empty($page)) {
     session_destroy();
     $page = 1;
 }
-if (!isset($_SESSION['score'])) {
-    $_SESSION['score'] = 0;
-}
-// Show which question they are on
-echo '<p class="breadcrumbs">Question ' . $page . ' of ' . $total . ' </p>';
-   echo '<p class="quiz">What is ' . $questions[$page - 1]['leftAdder'] . ' + ' . $questions[$page - 1]['rightAdder'] . '?</p>';
-   echo '<form action="index.php?p=' . ($page + 1) . '" method="post">';
-   echo '<input type="hidden" name="correctAnswer" value="' . $questions[$page - 1]['correctAnswer'] . '" />';
-
-// Show random question
-  echo '<div id="quiz-box">';
-
-// Shuffle answer buttons
-function checkAnswer() {
-    $answers = [];
-    if (isset($_POST['answer1'])) {
-        $_SESSION['answer1'] = filter_input(INPUT_POST, 'answer1', FILTER_SANITIZE_NUMBER_INT);
-        $answers = [$_SESSION['answer1']];
-    } elseif (isset($_POST['answer2'])) {
-        $_SESSION['answer2'] = filter_input(INPUT_POST, 'answer2', FILTER_SANITIZE_NUMBER_INT);
-        $answers = [$_SESSION['answer2']];
-    } elseif (isset($_POST['answer3'])) {
-        $_SESSION['answer3'] = filter_input(INPUT_POST, 'answer3', FILTER_SANITIZE_NUMBER_INT);
-        $answers = [$_SESSION['answer3']];
-    }
-    if (isset($_POST['correct'])) {
-        $_SESSION['correct'] = filter_input(INPUT_POST, 'correct', FILTER_SANITIZE_NUMBER_INT);
-    }
-    foreach ($answers as $answer) {
-        if ($answer == $_SESSION['correct']) {
-            echo "<p class='breadcrumbs'>Correct!</p>";
-            ++$_SESSION['score'];
-        } else {
-            echo "<p class='breadcrumbs'>That is incorrect! The right answer was " . $_SESSION['correct'] . ".</p>";
-        }
-    }
-    return $_SESSION['score'];
-}
-function restart() {
-    echo "<form action='index.php' method='post'>";
-    echo "<input type='submit' class='btn' name='restart' value='Take the quiz again'>";
-    echo "</form>";
+$total = count($questions);
+// Show score
+if ($page <= 10) {
+    // Show random question
+    echo '<div id="quiz-box">';
+ 
+    // Show which question they are on
+    echo '<p class="breadcrumbs">Question ' . $page . ' of ' . $total . ' </p>';
+    echo '<p class="quiz">What is ' . $questions[$page - 1]['leftAdder'] . ' + ' . $questions[$page - 1]['rightAdder'] . '?</p>';
+    echo '<form action="index.php?p=' . ($page + 1) . '" method="post">';
+    echo '<input type="hidden" name="correctAnswer" value="' . $questions[$page - 1]['correctAnswer'] . '" />';
+    
+ 
+ // Shuffle answer buttons
+    $answers = [
+        $questions[$page - 1]['correctAnswer'], 
+        $questions[$page - 1]['firstIncorrectAnswer'], 
+        $questions[$page - 1]['secondIncorrectAnswer']
+    ];
+  
+  
+  
+    shuffle($answers);
+    echo '<input type="submit" class="btn" name="answer" value="' . $answers[0] . '" />';
+    echo '<input type="submit" class="btn" name="answer" value="' . $answers[1] . '" />';
+    echo '<input type="submit" class="btn" name="answer" value="' . $answers[2] . '" />';
+    echo '</form>';
+    echo '</div>';
+} else if ($page == 11) {
+    echo '<h1 class="quiz">The Quiz is Finished</h1>';
+    echo '<form action="index.php?p=' . ($page + 1) . '" method="post">';
+    echo '<input type="submit" class="btn" name="answer" value="Check Your Grade" />';
+    echo '</form>';
+} else if ($page == 12) {
+    echo '<h1 class="quiz">You got an</h1>';
+    echo '<p>You got '. $_SESSION['amountCorrect'] . ' out of ' . $total . ' questions!</p>';
+    echo '<form action="index.php">';
+    echo '<input type="submit" class="btn" name="quiz" value="You Will Need to Retake This Quiz" />';
+    echo '</form>';
+    echo '<style>h1, p {color: orange;} p {font-size: 2rem;}</style>';
 }
 
-// Shows the score
-function score() {
-    $score = checkAnswer();
-    echo "<p class='quiz'>Your final score is " . $score . " out of 10.</p>";
+
+// Keep track of which questions have been asked
+// Keep track of answers
+if (isset($_POST['answer']) && isset($_POST['correctAnswer'])) {
+    $_SESSION['answer'] = filter_input(INPUT_POST, 'answer', FILTER_SANITIZE_NUMBER_INT);
+    $_SESSION['correctAnswer'] = filter_input(INPUT_POST, 'correctAnswer', FILTER_SANITIZE_NUMBER_INT);
+    if ($_SESSION['answer'] == $_SESSION['correctAnswer']) {
+        // Toast correct and incorrect answers
+        echo '<h2>Excellent! That is correct!</h2>';
+        $_SESSION['amountCorrect']++;
+    } else {
+        echo '<h2>Sorry, the answer was ' . $_SESSION['correctAnswer'] . ' .</h2>';
+    }
 }
 
 ?>
